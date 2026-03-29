ff1a893c75eeff368e3dcd3e2e2c471cb6f4f7d5
Luis Diego Roque Moya <luis.roquetic@gmail.com>
2026-03-29 20:21:01 -0300

    Add admin site configuration

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Course, Lesson, Instructor, Question, Choice, Enrollment, Submission
 
-# Register your models here.
+
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
+
+
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 5
+
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline, QuestionInline]
+    list_display = ['name', 'pub_date']
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title', 'order', 'course']
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Enrollment)
+admin.site.register(Submission)
